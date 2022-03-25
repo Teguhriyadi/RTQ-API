0f5bec0def6e6420a35ef401e08dba7d4fc0936b
hakim-asrori <saneglos005@gmail.com>
2022-03-25 20:16:27 +0700

    menambahkan fitur absensi pengajar

--- RumahTahfidz/app/Http/Controllers/AbsensiPengajarController.php
+++ RumahTahfidz/app/Http/Controllers/AbsensiPengajarController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+use App\Models\Absensi;
+
 class AbsensiPengajarController extends Controller
 {
     public function __construct()
@@ -16,8 +18,20 @@ class AbsensiPengajarController extends Controller
     {
         $id_pengajar = $request->user()->id;
 
-        
+        $this->validate($request, [
+            'gambar' => 'required',
+            'alamat' => 'required',
+        ]);
 
-        return response()->json($id_pengajar, 200);
+        $gambar = $request->post('gambar');
+        $alamat = $request->post('alamat');
+
+        Absensi::create([
+            'id_pengajar' => $id_pengajar,
+            'alamat' => $alamat,
+            'gambar' => $gambar,
+        ]);
+
+        return response()->json(['message' => 'Data success'], 201);
     }
 }
--- RumahTahfidz/app/Models/Absensi.php
+++ RumahTahfidz/app/Models/Absensi.php
@@ -9,22 +9,7 @@ class Absensi extends Model
 {
     use HasFactory;
 
-    protected $table = "tb_absensi";
+    protected $table = "tb_absensi_pengajar";
 
     protected $guarded = ['created_at', 'updated_at'];
-
-    public function getSiswa()
-    {
-        return $this->hasOne(Siswa::class, "id", 'id_siswa');
-    }
-
-    public function getPengajar()
-    {
-        return $this->hasOne(Pengajar::class, "id", "id_pengajar");
-    }
-
-    public function getStatusAbsen()
-    {
-        return $this->hasOne(StatusAbsen::class, "id", "status_absen");
-    }
 }
