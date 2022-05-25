c2e5a756ae84bd15c6d6824bab06f23ceb1b6035
Teguhriyadi <ilham.teguh55@gmail.com>
2022-05-25 14:02:02 +0700

    Mengubah Nama Route

--- RumahTahfidz/routes/web.php
+++ RumahTahfidz/routes/web.php
@@ -32,11 +32,11 @@ $router->post('api-v1/login/', 'AuthController@login');
 // Detail User
 $router->get('api-v1/profil/user/detail', 'ProfilController@detail');
 
-// Kategori Penilaian
-$router->get('api-v1/kategori_penilaian/view/all', 'KategoriPenilaianController@view');
+// Kategori Pelajaran
+$router->get('api-v1/kategori/pelajaran/view/all', 'KategoriPenilaianController@view');
 
-$router->get('api-v1/kategori/pelajaran/view/all', 'KategoriPelajaranController@view');
-$router->get('api-v1/kategori/pelajaran/view/{id_kategori_penilaian}/{id_jenjang}', 'KategoriPelajaranController@viewByKategoriNJenjang');
+$router->get('api-v1/pelajaran/view/all', 'KategoriPelajaranController@view');
+$router->get('api-v1/pelajaran/view/{id_kategori_penilaian}/{id_jenjang}', 'KategoriPelajaranController@viewByKategoriNJenjang');
 // List Jenjang
 $router->get('api-v1/jenjang/view/all', 'JenjangController@view');
 
