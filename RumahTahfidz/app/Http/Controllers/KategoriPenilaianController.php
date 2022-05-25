9da12646bc1c67c585a7b1d9ed4f5215388225c4
Teguhriyadi <ilham.teguh55@gmail.com>
2022-05-25 15:14:36 +0700

    PUSH

--- RumahTahfidz/app/Http/Controllers/KategoriPenilaianController.php
+++ RumahTahfidz/app/Http/Controllers/KategoriPenilaianController.php
@@ -28,9 +28,9 @@ class KategoriPenilaianController extends Controller
         return response()->json($data, 200);
     }
 
-    public function viewByJenjangNPenilaian($id_jenjang, $id_kategori_penilaian)
+    public function viewByJenjangNPenilaian($id_jenjang, $id_katagori)
     {
-        $data = KategoriPelajaran::where("id_jenjang", $id_jenjang)->where("id_kategori_penilaian", $id_kategori_penilaian)->get();
+        $data = KategoriPelajaran::where("id_jenjang", $id_jenjang)->where("id_kategori_penilaian", $id_katagori)->get();
 
         $d = [];
 
