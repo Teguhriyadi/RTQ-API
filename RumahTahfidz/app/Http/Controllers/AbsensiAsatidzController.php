88056bc3a22a411754edfb3e9dc91c1a6d37988a
Teguhriyadi <ilham.teguh55@gmail.com>
2022-06-12 15:04:06 +0700

    PUSH

--- RumahTahfidz/app/Http/Controllers/AbsensiAsatidzController.php
+++ RumahTahfidz/app/Http/Controllers/AbsensiAsatidzController.php
@@ -45,8 +45,8 @@ class AbsensiAsatidzController extends Controller
 
         $asatidz = Str::random(32);
 
-        if ($request->hasFile('image')) {
-            $request->file('image')->move('assets/absensi/asatidz/' . date('Y_m_d'), $asatidz);
+        if ($request->hasFile('gambar')) {
+            $request->file('gambar')->move('assets/absensi/asatidz/' . date('Y_m_d'), $asatidz);
             Absensi::create([
                 'gambar' => url() . '/assets/absensi/asatidz/' . date('Y_m_d') . '/' . $asatidz,
                 'alamat' => $request->alamat,
